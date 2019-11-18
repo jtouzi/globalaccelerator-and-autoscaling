20328028520cf8e7d5bf430e9c2fe1a7352cb91e
Jibril Touzi <jtouzi@gmail.com>
2019-11-18 01:50:15 -0600

    Update autoscaling_globalaccelerator.py

--- autoscaling_globalaccelerator.py
+++ autoscaling_globalaccelerator.py
@@ -18,14 +18,14 @@ logger.setLevel(logging.DEBUG)
 aga_client = boto3.client('globalaccelerator')
 
 # Accelerator Parameters
-ENDPOINT_GROUP_ARN = "arn:aws:globalaccelerator::012345678901:accelerator/c9d8f18d-e6a7-4f28-ae95-261507146530/listener/461df876/endpoint-group/c3770cbbf005"
+ENDPOINT_GROUP_ARN = "arn:aws:globalaccelerator::071855492661:accelerator/c9d8f18d-e6a7-4f28-ae95-261507146530/listener/461df876/endpoint-group/c3770cbbf005"
 ENDPOINT_WEIGHT = 128
 CLIENT_IP_PRESERVATION = True
 
 def lambda_handler(event, context):
     # print(("Received event: " + json.dumps(event, indent=2)))
-    Lifecycle_Hook_Name = event['LifecycleHookName'] # "ASG-AGA-Hook-Launching" OR "ASG-AGA-Hook-Terminaning",
-    ec2_instance = event['EC2InstanceId']
+    Lifecycle_Hook_Name = event['details']['LifecycleHookName'] # "ASG-AGA-Launching" OR "ASG-AGA-Terminaning",
+    ec2_instance = event['details']['EC2InstanceId']
     # return detail_type
     
     response = aga_client.describe_endpoint_group(
@@ -34,13 +34,13 @@ def lambda_handler(event, context):
 
     endpoints = []
     
-    if Lifecycle_Hook_Name == 'ASG-AGA-Hook-Launching': # Add the endpoint to the Accelerator
+    if Lifecycle_Hook_Name == 'ASG-AGA-Launching': # Add the endpoint to the Accelerator
         for EndpointID in response['EndpointGroup']['EndpointDescriptions']:
             result = {'EndpointId': EndpointID['EndpointId'],'Weight': EndpointID['Weight'],'ClientIPPreservationEnabled': CLIENT_IP_PRESERVATION}
             endpoints.append(result)
         # Endpoint to add
         endpoints.append({'EndpointId': ec2_instance,'Weight': ENDPOINT_WEIGHT,'ClientIPPreservationEnabled': CLIENT_IP_PRESERVATION})
-    elif Lifecycle_Hook_Name == 'ASG-AGA-Hook-Terminaning': # Remove the endpoint from the Accelerator
+    elif Lifecycle_Hook_Name == 'ASG-AGA-Terminaning': # Remove the endpoint from the Accelerator
         for EndpointID in response['EndpointGroup']['EndpointDescriptions']:
             if EndpointID['EndpointId'] != ec2_instance:
                 result = {'EndpointId': EndpointID['EndpointId'],'Weight': EndpointID['Weight'],'ClientIPPreservationEnabled': CLIENT_IP_PRESERVATION}
